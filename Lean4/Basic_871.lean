3480c7c13b4bcd4da7282609cc2907e8964be0f6
Nishikapa <32427757+Nishikapa@users.noreply.github.com>
2026-02-10 07:12:12 +0000

    885まで完了

--- Lean4/Basic_871.lean
+++ Lean4/Basic_871.lean
@@ -218,16 +218,27 @@ theorem ex873 (R S T : WRel α β) :
 -- 874：wOr は冪等（R ∨ R = R の 0/1 版）
 theorem ex874 (R : WRel α β) :
     wOr R R = wBool R := by
-  -- TODO
   -- ヒント：support が同じことを示して wBool に落とす（ex796 / ex816）
-  sorry
+  funext a1 b1
+  dsimp [wOr, wAdd, wBool, maskW, wSupp]
+  by_cases h1: R a1 b1 > 0
+  rw [if_pos h1]
+  rw [if_pos]
+  rw [gt_iff_lt]
+  rw [Nat.add_pos_iff_pos_or_pos]
+  left
+  exact h1
+  obtain h2 :=
+    Nat.eq_zero_of_not_pos h1
+  clear h1
+  rw [h2]
 
 -- 875：wZero は wOr の単位元（ただし結果は wBool R）
 theorem ex875 (R : WRel α β) :
     wOr R (wZero α β) = wBool R := by
-  -- TODO
   -- ヒント：wAdd R 0 = R, wOr 定義
-  sorry
+  funext a1 b1
+  dsimp [wOr, wAdd, wBool, wZero, maskW, wSupp]
 
 --------------------------------------------------------------------------------
 -- 876〜880：wReachComp と wOr の相互作用（分配・zero）
@@ -238,39 +249,257 @@ theorem ex876 (keys : List β) (R R' : WRel α β) (S : WRel β γ) :
     wReachComp keys (wOr R R') S
       =
     wOr (wReachComp keys R S) (wReachComp keys R' S) := by
-  -- TODO
   -- ヒント：wReachComp は support だけを見る（ex818） + ex871
-  sorry
+  funext a1 c1
+  dsimp [wReachComp, wOr, wSupp, relCompList, relAdd, maskW, wBool, wAdd]
+
+  by_cases h1 :
+    ∃ b, b ∈ keys ∧ (if R a1 b + R' a1 b > 0 then 1 else 0) > 0 ∧ S b c1 > 0
+  -- pos
+  obtain ⟨b1, h2, h3, h4⟩ := h1
+  have h3_1 : R a1 b1 + R' a1 b1 > 0 := by
+    by_cases h_ : R a1 b1 + R' a1 b1 > 0
+    exact h_
+    rw [if_neg h_] at h3
+    contradiction
+  clear h3
+
+  rw [gt_iff_lt] at h3_1
+  rw [Nat.add_pos_iff_pos_or_pos] at h3_1
+  rw [if_pos]
+  rw [if_pos]
+  rw [gt_iff_lt]
+  rw [Nat.add_pos_iff_pos_or_pos]
+  obtain h3_1_1 | h3_1_2 := h3_1
+  left
+  rw [if_pos]
+  apply Nat.zero_lt_one
+  exists b1
+  right
+  rw [if_pos]
+  apply Nat.zero_lt_one
+  exists b1
+  exists b1
+  constructor
+  exact h2
+  rw [if_pos]
+  constructor
+  apply Nat.zero_lt_one
+  exact h4
+  rw [gt_iff_lt]
+  rw [Nat.add_pos_iff_pos_or_pos]
+  obtain h3_2 | h3_3 := h3_1
+  left
+  exact h3_2
+  right
+  exact h3_3
+  rw [if_neg]
+  rw [if_neg]
+  intro h5
+  apply h1
+  clear h1
+  rw [gt_iff_lt] at h5
+  rw [Nat.add_pos_iff_pos_or_pos] at h5
+  obtain h5_1 | h5_2 := h5
+  have h5_3 : ∃ b, b ∈ keys ∧ R a1 b > 0 ∧ S b c1 > 0 := by
+    by_cases h_ : ∃ b, b ∈ keys ∧ R a1 b > 0 ∧ S b c1 > 0
+    exact h_
+    rw [if_neg h_] at h5_1
+    contradiction
+  clear h5_1
+  obtain ⟨b1, h6, h7, h8⟩ := h5_3
+  exists b1
+  constructor
+  exact h6
+  rw [if_pos]
+  constructor
+  apply Nat.zero_lt_one
+  exact h8
+  rw [gt_iff_lt]
+  rw [Nat.add_pos_iff_pos_or_pos]
+  left
+  exact h7
+  have h5_4 : ∃ b, b ∈ keys ∧ R' a1 b > 0 ∧ S b c1 > 0 := by
+    by_cases h_ : ∃ b, b ∈ keys ∧ R' a1 b > 0 ∧ S b c1 > 0
+    exact h_
+    rw [if_neg h_] at h5_2
+    contradiction
+  clear h5_2
+  obtain ⟨b1, h6, h7, h8⟩ := h5_4
+  exists b1
+  constructor
+  exact h6
+  rw [if_pos]
+  constructor
+  apply Nat.zero_lt_one
+  exact h8
+  rw [gt_iff_lt]
+  rw [Nat.add_pos_iff_pos_or_pos]
+  right
+  exact h7
+  intro h9
+  apply h1
+  clear h1
+  obtain ⟨b2, h10, h11, h12⟩ := h9
+  have h11_1 : R a1 b2 + R' a1 b2 > 0 := by
+    by_cases h_ : R a1 b2 + R' a1 b2 > 0
+    exact h_
+    rw [if_neg h_] at h11
+    contradiction
+  clear h11
+  exists b2
+  rw [if_pos]
+  constructor
+  exact h10
+  constructor
+  apply Nat.zero_lt_one
+  exact h12
+  exact h11_1
 
 -- 877：reach は wOr（右）に分配する
 theorem ex877 (keys : List β) (R : WRel α β) (S S' : WRel β γ) :
     wReachComp keys R (wOr S S')
       =
     wOr (wReachComp keys R S) (wReachComp keys R S') := by
-  -- TODO
+
   -- ヒント：wReachComp は support だけを見る（ex818） + ex871
-  sorry
+  funext a1 c1
+  dsimp [wReachComp, wOr, wSupp, relCompList, relAdd, maskW, wBool, wAdd]
+
+  by_cases h1 :
+    ∃ b, b ∈ keys ∧ R a1 b > 0 ∧ (if S b c1 + S' b c1 > 0 then 1 else 0) > 0
+
+  -- pos
+  obtain ⟨b1, h2, h3, h4⟩ := h1
+  have h4_1 : S b1 c1 + S' b1 c1 > 0 := by
+    by_cases h_ : S b1 c1 + S' b1 c1 > 0
+    exact h_
+    rw [if_neg h_] at h4
+    contradiction
+  clear h4
+  rw [gt_iff_lt] at h4_1
+  rw [Nat.add_pos_iff_pos_or_pos] at h4_1
+  rw [if_pos]
+  rw [if_pos]
+  rw [gt_iff_lt]
+  rw [Nat.add_pos_iff_pos_or_pos]
+  obtain h4_2 | h4_3 := h4_1
+  left
+  rw [if_pos]
+  apply Nat.zero_lt_one
+  exists b1
+  right
+  rw [if_pos]
+  apply Nat.zero_lt_one
+  exists b1
+  exists b1
+  constructor
+  exact h2
+  constructor
+  exact h3
+  rw [if_pos]
+  apply Nat.zero_lt_one
+  rw [gt_iff_lt]
+  rw [Nat.add_pos_iff_pos_or_pos]
+  exact h4_1
+  rw [if_neg]
+  rw [if_neg]
+  intro h5
+  apply h1
+  rw [gt_iff_lt] at h5
+  rw [Nat.add_pos_iff_pos_or_pos] at h5
+  obtain h5_1 | h5_2 := h5
+  have h5_3 : ∃ b, b ∈ keys ∧ R a1 b > 0 ∧ S b c1 > 0 := by
+    by_cases h_ : ∃ b, b ∈ keys ∧ R a1 b > 0 ∧ S b c1 > 0
+    exact h_
+    rw [if_neg h_] at h5_1
+    contradiction
+  clear h5_1
+  obtain ⟨b1, h6, h7, h8⟩ := h5_3
+  exists b1
+  constructor
+  exact h6
+  constructor
+  exact h7
+  rw [if_pos]
+  apply Nat.zero_lt_one
+  rw [gt_iff_lt]
+  rw [Nat.add_pos_iff_pos_or_pos]
+  left
+  exact h8
+  have h5_4 : ∃ b, b ∈ keys ∧ R a1 b > 0 ∧ S' b c1 > 0 := by
+    by_cases h_ : ∃ b, b ∈ keys ∧ R a1 b > 0 ∧ S' b c1 > 0
+    exact h_
+    rw [if_neg h_] at h5_2
+    contradiction
+  clear h5_2
+  obtain ⟨b1, h6, h7, h8⟩ := h5_4
+  exists b1
+  constructor
+  exact h6
+  constructor
+  exact h7
+  rw [if_pos]
+  apply Nat.zero_lt_one
+  rw [gt_iff_lt]
+  rw [Nat.add_pos_iff_pos_or_pos]
+  right
+  exact h8
+  intro h9
+  apply h1
+  obtain ⟨b2, h10, h11, h12⟩ := h9
+  have h12_1 : S b2 c1 + S' b2 c1 > 0 := by
+    by_cases h_ : S b2 c1 + S' b2 c1 > 0
+    exact h_
+    rw [if_neg h_] at h12
+    contradiction
+  clear h12
+  exists b2
+  constructor
+  exact h10
+  constructor
+  exact h11
+  rw [if_pos]
+  apply Nat.zero_lt_one
+  exact h12_1
 
 -- 878：zero と reach（左が 0）
 theorem ex878 (keys : List β) (S : WRel β γ) :
     wReachComp keys (wZero α β) S = wZero α γ := by
-  -- TODO
   -- ヒント：wSupp(wZero)=False → relCompList が空
-  sorry
+  funext a1 c1
+  dsimp [wReachComp, wZero, wSupp, relCompList, maskW]
+  rw [if_neg]
+  intro h1
+  obtain ⟨b1, h2, h3, h4⟩ := h1
+  contradiction
 
 -- 879：zero と reach（右が 0）
 theorem ex879 (keys : List β) (R : WRel α β) :
     wReachComp keys R (wZero β γ) = wZero α γ := by
-  -- TODO
   -- ヒント：wSupp(wZero)=False → relCompList が空
-  sorry
+  funext a1 c1
+  dsimp [wReachComp, wZero, wSupp, relCompList, maskW]
+  rw [if_neg]
+  intro h1
+  obtain ⟨b1, h2, h3, h4⟩ := h1
+  contradiction
 
 -- 880：reach の結果は常に 0/1（wBool の固定点）
 theorem ex880 (keys : List β) (R : WRel α β) (S : WRel β γ) :
     wBool (wReachComp keys R S) = wReachComp keys R S := by
-  -- TODO
   -- ヒント：wReachComp は maskW で定義されているので ex812
-  sorry
+  funext a1 c1
+  dsimp [wReachComp, wBool, maskW, wSupp, relCompList]
+  by_cases h1 :
+    ∃ b, b ∈ keys ∧ R a1 b > 0 ∧ S b c1 > 0
+  rw [if_pos h1]
+  rw [if_pos]
+  apply Nat.zero_lt_one
+  rw [if_neg h1]
+  rw [if_neg]
+  intro h2
+  contradiction
 
 --------------------------------------------------------------------------------
 -- 881〜885：wsumW と reach（OR-和との分配・graph 特別ケース）
@@ -282,43 +511,255 @@ theorem ex881 (keysβ : List β) (keysι : List ι)
     wReachComp keysβ (wsumW keysι F) S
       =
     wsumW keysι (fun i => wReachComp keysβ (F i) S) := by
-  -- TODO
   -- ヒント：support で示すのが楽：ex818 + ex846
-  sorry
+  funext a1 c1
+  dsimp [wReachComp, wsumW, wSupp, relCompList, maskW, wBool, wAdd]
+  conv =>
+    conv =>
+      lhs
+      arg 1
+      arg 1
+      intro b1
+      rhs
+      arg 1
+      arg 1
+      arg 1
+      rw [ex606]
+    conv =>
+      rhs
+      arg 1
+      rw [ex606]
+
+  by_cases h1 :
+    ∃ b1, b1 ∈ keysβ ∧ (if ∃ x, x ∈ keysι ∧ F x a1 b1 > 0 then 1 else 0) > 0 ∧ S b1 c1 > 0
+
+  -- pos
+  rw [if_pos h1]
+  obtain ⟨b2, h2, h3, h4⟩ := h1
+  have h3_1 : ∃ x, x ∈ keysι ∧ F x a1 b2 > 0 := by
+    by_cases h_ : ∃ x, x ∈ keysι ∧ F x a1 b2 > 0
+    exact h_
+    rw [if_neg h_] at h3
+    contradiction
+  clear h3
+  obtain ⟨i1, h5, h6⟩ := h3_1
+  rw [if_pos]
+  exists i1
+  constructor
+  exact h5
+  rw [if_pos]
+  apply Nat.zero_lt_one
+  exists b2
+
+  -- neg
+  rw [if_neg h1]
+  rw [if_neg]
+  intro h6
+  apply h1
+  obtain ⟨b2, h7, h8⟩ := h6
+  have h8_1 : ∃ b, b ∈ keysβ ∧ F b2 a1 b > 0 ∧ S b c1 > 0 := by
+    by_cases h_ : ∃ b, b ∈ keysβ ∧ F b2 a1 b > 0 ∧ S b c1 > 0
+    exact h_
+    rw [if_neg h_] at h8
+    contradiction
+  clear h8
+  obtain ⟨b3, h9, h10, h11⟩ := h8_1
+  exists b3
+  constructor
+  exact h9
+  constructor
+  rw [if_pos]
+  apply Nat.zero_lt_one
+  exists b2
+  exact h11
 
 -- 882：keys がすべて含むなら wId は本当の右単位になる
 theorem ex882 (keysβ : List β) (R : WRel α β) :
     (∀ b : β, b ∈ keysβ) →
       wReachComp keysβ R (wId β) = wBool R := by
-  -- TODO
   -- ヒント：ex842 の RHS で b∈keysβ を消す
-  sorry
+  intro h1
+  funext a1 b1
+  dsimp [wReachComp, wId, wBool, wSupp, relCompList, maskW, relId]
+  by_cases h2 :
+    ∃ b, b ∈ keysβ ∧ R a1 b > 0 ∧ (if b = b1 then 1 else 0) > 0
+
+  -- pos
+  rw [if_pos h2]
+  obtain ⟨b2, h3, h4, h5⟩ := h2
+  have h5_1 : b2 = b1 := by
+    by_cases h_ : b2 = b1
+    exact h_
+    rw [if_neg h_] at h5
+    contradiction
+  clear h5
+  rw [if_pos]
+  rw [←h5_1]
+  exact h4
+
+  -- neg
+  rw [if_neg h2]
+  rw [if_neg]
+  intro h6
+  apply h2
+  exists b1
+  obtain h7 := h1 b1
+  constructor
+  exact h7
+  constructor
+  exact h6
+  rw [if_pos]
+  apply Nat.zero_lt_one
+  rfl
 
 -- 883：keys がすべて含むなら wId は本当の左単位になる
 theorem ex883 (keysα : List α) (R : WRel α β) :
     (∀ a : α, a ∈ keysα) →
       wReachComp keysα (wId α) R = wBool R := by
-  -- TODO
   -- ヒント：ex841 の RHS で a∈keysα を消す
-  sorry
+  intro h1
+  funext a1 b1
+  dsimp [wReachComp, wId, wBool, wSupp, relCompList, maskW, relId]
+  by_cases h2 :
+    ∃ b, b ∈ keysα ∧ (if a1 = b then 1 else 0) > 0 ∧ R b b1 > 0
+
+  -- pos
+  rw [if_pos h2]
+  obtain ⟨b2, h3, h4, h5⟩ := h2
+  have h4_1 : a1 = b2 := by
+    by_cases h_ : a1 = b2
+    exact h_
+    rw [if_neg h_] at h4
+    contradiction
+  clear h4
+  rw [if_pos]
+  rw [h4_1]
+  exact h5
+
+  -- neg
+  rw [if_neg h2]
+  rw [if_neg]
+  intro h6
+  apply h2
+  exists a1
+  obtain h7 := h1 a1
+  constructor
+  exact h7
+  constructor
+  rw [if_pos]
+  apply Nat.zero_lt_one
+  rfl
+  exact h6
 
 -- 884：graph を右に置いた reach の support（存在条件）
 theorem ex884 (keys : List β) (R : WRel α β) (g : β → γ) :
     wSupp (wReachComp keys R (wGraph g))
       =
     (fun a c => ∃ b, b ∈ keys ∧ wSupp R a b ∧ g b = c) := by
-  -- TODO
   -- ヒント：ex818 + wSupp(wGraph g)=relGraph g
-  sorry
+  funext a1 c1
+  dsimp [wReachComp, wGraph, wSupp, relCompList, relGraph, maskW]
+  by_cases h1 : ∃ b, b ∈ keys ∧ R a1 b > 0 ∧ (if g b = c1 then 1 else 0) > 0
+
+  -- pos
+  rw [if_pos h1]
+  obtain ⟨b1, h2, h3, h4⟩ := h1
+  have h4_1 : g b1 = c1 := by
+    by_cases h_ : g b1 = c1
+    exact h_
+    rw [if_neg h_] at h4
+    contradiction
+  clear h4
+  apply propext
+  constructor
+  intro h5
+  exists b1
+  intro h6
+  apply Nat.zero_lt_one
+
+  -- neg
+  rw [if_neg h1]
+  apply propext
+  constructor
+  intro h7
+  contradiction
+  intro h8
+  obtain ⟨b2, h9, h10, h11⟩ := h8
+  apply False.elim
+  apply h1
+  exists b2
+  constructor
+  exact h9
+  constructor
+  exact h10
+  rw [if_pos]
+  apply Nat.zero_lt_one
+  exact h11
 
 -- 885：graph を右に置いた reach は “列の行選択” の OR-和（Basic_841 の復習）
 theorem ex885 (keys : List β) (R : WRel α β) (g : β → γ) :
     wReachComp keys R (wGraph g)
       =
     wsumW keys (fun b =>
-      wMask (fun a c => wBool R a b) (fun _ c => g b = c)) := by
-  -- TODO
+      wMask (fun a _ => wBool R a b) (fun _ c => g b = c)) := by
   -- ヒント：Basic_841.ex843
-  sorry
+  funext a1 c1
+  dsimp [wReachComp, wGraph, wsumW, wMask, wBool, wSupp, relCompList, relGraph, maskW, wAdd]
+  conv =>
+    rhs
+    arg 1
+    rw [ex606]
+
+  by_cases h1 : ∃ b, b ∈ keys ∧ R a1 b > 0 ∧ (if g b = c1 then 1 else 0) > 0
+
+  -- pos
+  rw [if_pos h1]
+  obtain ⟨b2, h2, h3, h4⟩ := h1
+  have h4_1 : g b2 = c1 := by
+    by_cases h_ : g b2 = c1
+    exact h_
+    rw [if_neg h_] at h4
+    contradiction
+  clear h4
+  rw [if_pos]
+  exists b2
+  constructor
+  exact h2
+  rw [gt_iff_lt]
+  apply Nat.mul_pos
+  rw [if_pos]
+  apply Nat.zero_lt_one
+  exact h3
+  rw [if_pos]
+  apply Nat.zero_lt_one
+  exact h4_1
+
+  -- neg
+  rw [if_neg h1]
+  rw [if_neg]
+  intro h5
+  apply h1
+  obtain ⟨b2, h6, h7⟩ := h5
+  rw [gt_iff_lt] at h7
+  have  h7_1 : 0 < R a1 b2 := by
+    by_cases h_ : 0 < R a1 b2
+    exact h_
+    rw [if_neg h_] at h7
+    rw [Nat.zero_mul] at h7
+    contradiction
+  have  h7_2 : g b2 = c1 := by
+    by_cases h_ : g b2 = c1
+    exact h_
+    rw [if_neg h_] at h7
+    contradiction
+  clear h7
+  exists b2
+  constructor
+  exact h6
+  constructor
+  exact h7_1
+  rw [if_pos]
+  apply Nat.zero_lt_one
+  exact h7_2
 
 end TL
--- Lean4/Basic_886.lean
+++ Lean4/Basic_886.lean
@@ -43,36 +43,185 @@ theorem ex886 (R S : WRel A B) :
 -- 887: wAnd is commutative.
 theorem ex887 (R S : WRel A B) :
     wAnd R S = wAnd S R := by
-  -- TODO
   -- Hint: unfold wAnd, then use ex668 (wMul commutative).
-  sorry
+  funext a1 b1
+  dsimp [wAnd, wBool, maskW, wSupp, wMul]
+  by_cases h1 : R a1 b1 * S a1 b1 > 0
+
+  -- pos
+  rw [if_pos h1]
+  rw [if_pos]
+  rw [Nat.mul_comm]
+  exact h1
+
+  -- neg
+  rw [if_neg h1]
+  rw [if_neg]
+  rw [Nat.mul_comm]
+  exact h1
 
 -- 888: wAnd is associative.
 theorem ex888 (R S T : WRel A B) :
     wAnd (wAnd R S) T = wAnd R (wAnd S T) := by
-  -- TODO
   -- Hint:
   --   * compare supports via ex886
   --   * use relMul associativity pointwise
   --   * finish with maskW / wBool extensionality
-  sorry
+  funext a1 b1
+  dsimp [wAnd, wBool, maskW, wSupp, wMul]
+  by_cases h1 : (if R a1 b1 * S a1 b1 > 0 then 1 else 0) * T a1 b1 > 0
+  -- pos
+  rw [gt_iff_lt] at h1
+  obtain h1_1 := Nat.pos_of_mul_pos_left h1
+  have h1_2 : R a1 b1 * S a1 b1 > 0 := by
+    by_cases h_ : R a1 b1 * S a1 b1 > 0
+    exact h_
+    rw [if_neg h_] at h1
+    rw [Nat.zero_mul] at h1
+    contradiction
+  clear h1
+  obtain h1_3 := Nat.pos_of_mul_pos_left h1_2
+  obtain h1_4 := Nat.pos_of_mul_pos_right h1_2
+  clear h1_2
+  rw [if_pos]
+  rw [if_pos]
+  rw [if_pos]
+  rw [Nat.mul_one]
+  exact h1_4
+  rw [gt_iff_lt]
+  apply Nat.mul_pos
+  exact h1_3
+  exact h1_1
+  rw [if_pos]
+  rw [Nat.one_mul]
+  exact h1_1
+  rw [gt_iff_lt]
+  apply Nat.mul_pos
+  exact h1_4
+  exact h1_3
+
+  -- neg
+  rw [if_neg]
+  rw [if_neg]
+  intro h2
+  rw [gt_iff_lt] at h2
+
+  --obtain h2_1 := Nat.pos_of_mul_pos_left h2
+  have h2_1 := Nat.pos_of_mul_pos_right h2
+  have h2_2 : S a1 b1 * T a1 b1 > 0 := by
+    by_cases h_ : S a1 b1 * T a1 b1 > 0
+    exact h_
+    rw [if_neg h_] at h2
+    rw [Nat.mul_zero] at h2
+    contradiction
+  clear h2
+  rw [gt_iff_lt] at h2_2
+  obtain h2_3 := Nat.pos_of_mul_pos_left h2_2
+  obtain h2_4 := Nat.pos_of_mul_pos_right h2_2
+  clear h2_2
+
+  apply h1
+  rw [gt_iff_lt]
+  apply Nat.mul_pos
+  rw [if_pos]
+  apply Nat.zero_lt_one
+  rw [gt_iff_lt]
+  apply Nat.mul_pos
+  exact h2_1
+  exact h2_4
+  exact h2_3
+  intro h3
+  apply h1
+  rw [if_pos]
+  rw [Nat.one_mul]
+  rw [gt_iff_lt] at h3
+  obtain h3_1 := Nat.pos_of_mul_pos_left h3
+  exact h3_1
+  obtain h3_2 := Nat.pos_of_mul_pos_right h3
+  have h3_3 : R a1 b1 * S a1 b1 > 0 := by
+    by_cases h_ : R a1 b1 * S a1 b1 > 0
+    exact h_
+    rw [if_neg h_] at h3
+    rw [Nat.zero_mul] at h3
+    contradiction
+  exact h3_3
 
 -- 889: idempotence (AND with itself collapses to bool).
 theorem ex889 (R : WRel A B) :
     wAnd R R = wBool R := by
-  -- TODO
   -- Hint: unfold wAnd and use positivity of x*x.
-  sorry
+  funext a1 b1
+  dsimp [wAnd, wBool, maskW, wSupp, wMul]
+  by_cases h1 : R a1 b1 * R a1 b1 > 0
+  -- pos
+  rw [if_pos h1]
+  rw [if_pos]
+  rw [gt_iff_lt] at h1
+  obtain h1_1 := Nat.pos_of_mul_pos_left h1
+  exact h1_1
+  -- neg
+  rw [if_neg h1]
+  rw [if_neg]
+  intro h2
+  apply h1
+  rw [gt_iff_lt]
+  apply Nat.mul_pos
+  exact h2
+  exact h2
 
 -- 890: absorption with OR.
 theorem ex890 (R S : WRel A B) :
     wAnd R (wOr R S) = wBool R := by
-  -- TODO
   -- Hint:
   --   * support side is R /\ (R \/ S)
   --   * use ex871 for wOr support
   --   * conclude by extensionality on 0/1 relations
-  sorry
+  funext a1 b1
+  dsimp [wAnd, wOr, wBool, maskW, wSupp, wMul, wAdd]
+  by_cases h1 : (R a1 b1 * if R a1 b1 + S a1 b1 > 0 then 1 else 0) > 0
+  -- pos
+  rw [gt_iff_lt] at h1
+  obtain h1_2 := Nat.pos_of_mul_pos_right h1
+  clear h1
+  rw [if_pos]
+  rw [if_pos]
+  exact h1_2
+  rw [if_pos]
+  rw [Nat.mul_one]
+  exact h1_2
+  rw [gt_iff_lt]
+  rw [Nat.add_pos_iff_pos_or_pos]
+  left
+  exact h1_2
+  -- neg
+  rw [if_neg]
+  rw [if_neg]
+  intro h2
+  apply h1
+  rw [gt_iff_lt]
+  apply Nat.mul_pos
+  exact h2
+  rw [if_pos]
+  apply Nat.zero_lt_one
+  rw [gt_iff_lt]
+  rw [Nat.add_pos_iff_pos_or_pos]
+  left
+  exact h2
+  intro h3
+  apply h1
+  rw [gt_iff_lt]
+  apply Nat.mul_pos
+  rw [gt_iff_lt] at h3
+  obtain h3_1 := Nat.pos_of_mul_pos_right h3
+  exact h3_1
+  rw [if_pos]
+  apply Nat.zero_lt_one
+  rw [gt_iff_lt]
+  rw [gt_iff_lt] at h3
+  rw [Nat.add_pos_iff_pos_or_pos]
+  left
+  obtain h3_2 := Nat.pos_of_mul_pos_right h3
+  exact h3_2
 
 --------------------------------------------------------------------------------
 -- 891-895: ReachComp with wAnd (inequalities + singleton equality)
@@ -82,42 +231,373 @@ theorem ex890 (R S : WRel A B) :
 theorem ex891 (keys : List B) (R R' : WRel A B) (S : WRel B C) :
     WLe (wReachComp keys (wAnd R R') S)
         (wReachComp keys R S) := by
-  -- TODO
   -- Hint: unfold WLe; witness for left is also a witness for right.
-  sorry
+  dsimp [WLe]
+  intro a1 c1
+  dsimp [wReachComp, wAnd, wBool, wSupp, wMul, maskW, relCompList]
+  by_cases h1 :
+    ∃ b, b ∈ keys ∧ (if R a1 b * R' a1 b > 0 then 1 else 0) > 0 ∧ S b c1 > 0
+
+  -- pos
+  obtain ⟨b0, hkey, hR_and, hS⟩ := h1
+  have hR_and2 : R a1 b0 * R' a1 b0 > 0 := by
+    by_cases h_ : R a1 b0 * R' a1 b0 > 0
+    exact h_
+    rw [if_neg h_] at hR_and
+    contradiction
+  clear hR_and
+  rw [gt_iff_lt] at hR_and2
+  obtain hR_1 := Nat.pos_of_mul_pos_left hR_and2
+  obtain hR_2 := Nat.pos_of_mul_pos_right hR_and2
+  clear hR_and2
+  rw [if_pos]
+  rw [if_pos]
+  apply Nat.le_refl
+  exists b0
+  exists b0
+  constructor
+  exact hkey
+  constructor
+  rw [if_pos]
+  apply Nat.zero_lt_one
+  rw [gt_iff_lt]
+  apply Nat.mul_pos
+  exact hR_2
+  exact hR_1
+  exact hS
+
+  -- neg
+  rw [if_neg]
+  apply Nat.zero_le
+  intro h2
+  apply h1
+  obtain ⟨b0, hkey, hR_and, hS⟩ := h2
+  have hR_and2 : R a1 b0 * R' a1 b0 > 0 := by
+    by_cases h_ : R a1 b0 * R' a1 b0 > 0
+    exact h_
+    rw [if_neg h_] at hR_and
+    contradiction
+  obtain hR_and3 := Nat.pos_of_mul_pos_left hR_and2
+  obtain hR_and4 := Nat.pos_of_mul_pos_right hR_and2
+  clear hR_and
+  clear hR_and2
+  exists b0
+  constructor
+  exact hkey
+  constructor
+  rw [if_pos]
+  apply Nat.zero_lt_one
+  rw [gt_iff_lt]
+  apply Nat.mul_pos
+  exact hR_and4
+  exact hR_and3
+  exact hS
 
 -- 892: AND on the right input can only decrease reach.
 theorem ex892 (keys : List B) (R : WRel A B) (S S' : WRel B C) :
     WLe (wReachComp keys R (wAnd S S'))
         (wReachComp keys R S) := by
-  -- TODO
   -- Hint: same witness argument as ex891.
-  sorry
+  dsimp [WLe, wReachComp, wAnd, wBool, wSupp, wMul, maskW, relCompList]
+  intro a1 c1
+  by_cases h1 :
+    ∃ b, b ∈ keys ∧ R a1 b > 0 ∧ (if S b c1 * S' b c1 > 0 then 1 else 0) > 0
+  -- pos
+  obtain ⟨b0, hkey, hR, hS_and⟩ := h1
+  have hS_and2 : S b0 c1 * S' b0 c1 > 0 := by
+    by_cases h_ : S b0 c1 * S' b0 c1 > 0
+    exact h_
+    rw [if_neg h_] at hS_and
+    contradiction
+  clear hS_and
+  obtain hS_and3 := Nat.pos_of_mul_pos_left hS_and2
+  obtain hS_and4 := Nat.pos_of_mul_pos_right hS_and2
+  clear hS_and2
+  rw [if_pos]
+  rw [if_pos]
+  apply Nat.le_refl
+  exists b0
+  exists b0
+  constructor
+  exact hkey
+  constructor
+  exact hR
+  rw [if_pos]
+  apply Nat.zero_lt_one
+  rw [gt_iff_lt]
+  apply Nat.mul_pos
+  exact hS_and4
+  exact hS_and3
+  -- neg
+  rw [if_neg]
+  apply Nat.zero_le
+  intro h2
+  apply h1
+  obtain ⟨b0, hkey, hR, hS_and⟩ := h2
+  have hS_and2 : S b0 c1 * S' b0 c1 > 0 := by
+    by_cases h_ : S b0 c1 * S' b0 c1 > 0
+    exact h_
+    rw [if_neg h_] at hS_and
+    contradiction
+  clear hS_and
+  obtain hS_and3 := Nat.pos_of_mul_pos_left hS_and2
+  obtain hS_and4 := Nat.pos_of_mul_pos_right hS_and2
+  clear hS_and2
+  exists b0
+  constructor
+  exact hkey
+  constructor
+  exact hR
+  rw [if_pos]
+  apply Nat.zero_lt_one
+  rw [gt_iff_lt]
+  apply Nat.mul_pos
+  exact hS_and4
+  exact hS_and3
 
 -- 893: left-AND reach implies AND of the two reaches.
 theorem ex893 (keys : List B) (R R' : WRel A B) (S : WRel B C) :
     WLe (wReachComp keys (wAnd R R') S)
         (wAnd (wReachComp keys R S) (wReachComp keys R' S)) := by
-  -- TODO
   -- Hint: combine ex891 with the symmetric statement for R'.
-  sorry
+  dsimp [WLe, wReachComp, wAnd, wBool, wSupp, wMul, maskW, relCompList]
+  intro a1 c1
+  by_cases h1 : ∃ b, b ∈ keys ∧ (if R a1 b * R' a1 b > 0 then 1 else 0) > 0 ∧ S b c1 > 0
+  obtain ⟨b0, hkey, hR_and, hS⟩ := h1
+  have hR_and2 : R a1 b0 * R' a1 b0 > 0 := by
+    by_cases h_ : R a1 b0 * R' a1 b0 > 0
+    exact h_
+    rw [if_neg h_] at hR_and
+    contradiction
+  clear hR_and
+  obtain hR_1 := Nat.pos_of_mul_pos_left hR_and2
+  obtain hR_2 := Nat.pos_of_mul_pos_right hR_and2
+  clear hR_and2
+  rw [if_pos]
+  rw [if_pos]
+  apply Nat.le_refl
+  rw [if_pos]
+  rw [Nat.one_mul]
+  rw [if_pos]
+  apply Nat.zero_lt_one
+  exists b0
+  exists b0
+  exists b0
+  constructor
+  exact hkey
+  constructor
+  rw [if_pos]
+  apply Nat.zero_lt_one
+  rw [gt_iff_lt]
+  apply Nat.mul_pos
+  exact hR_2
+  exact hR_1
+  exact hS
+  rw [if_neg]
+  apply Nat.zero_le
+  intro h2
+  apply h1
+  obtain ⟨b0, hkey, hR_and, hS⟩ := h2
+  have hR_and2 : R a1 b0 * R' a1 b0 > 0 := by
+    by_cases h_ : R a1 b0 * R' a1 b0 > 0
+    exact h_
+    rw [if_neg h_] at hR_and
+    contradiction
+  clear hR_and
+  obtain hR_1 := Nat.pos_of_mul_pos_left hR_and2
+  obtain hR_2 := Nat.pos_of_mul_pos_right hR_and2
+  clear hR_and2
+  exists b0
+  constructor
+  exact hkey
+  constructor
+  rw [if_pos]
+  apply Nat.zero_lt_one
+  rw [gt_iff_lt]
+  apply Nat.mul_pos
+  exact hR_2
+  exact hR_1
+  exact hS
 
 -- 894: right-AND reach implies AND of the two reaches.
 theorem ex894 (keys : List B) (R : WRel A B) (S S' : WRel B C) :
     WLe (wReachComp keys R (wAnd S S'))
         (wAnd (wReachComp keys R S) (wReachComp keys R S')) := by
-  -- TODO
   -- Hint: same shape as ex893.
-  sorry
+  dsimp [WLe, wReachComp, wAnd, wBool, wSupp, wMul, maskW, relCompList]
+  intro a1 c1
+  by_cases h1 : ∃ b, b ∈ keys ∧ R a1 b > 0 ∧ (if S b c1 * S' b c1 > 0 then 1 else 0) > 0
+  obtain ⟨b0, hkey, hR, hS_and⟩ := h1
+  -- pos
+  have hS_and2 : S b0 c1 * S' b0 c1 > 0 := by
+    by_cases h_ : S b0 c1 * S' b0 c1 > 0
+    exact h_
+    rw [if_neg h_] at hS_and
+    contradiction
+  clear hS_and
+  obtain hS_and3 := Nat.pos_of_mul_pos_left hS_and2
+  obtain hS_and4 := Nat.pos_of_mul_pos_right hS_and2
+  clear hS_and2
+  rw [if_pos]
+  rw [if_pos]
+  apply Nat.le_refl
+  rw [if_pos]
+  rw [Nat.one_mul]
+  rw [if_pos]
+  apply Nat.zero_lt_one
+  exists b0
+  exists b0
+  exists b0
+  constructor
+  exact hkey
+  constructor
+  exact hR
+  rw [if_pos]
+  apply Nat.zero_lt_one
+  rw [gt_iff_lt]
+  apply Nat.mul_pos
+  exact hS_and4
+  exact hS_and3
+  -- neg
+  rw [if_neg]
+  apply Nat.zero_le
+  intro h2
+  apply h1
+  obtain ⟨b0, hkey, hR, hS_and⟩ := h2
+  have hS_and2 : S b0 c1 * S' b0 c1 > 0 := by
+    by_cases h_ : S b0 c1 * S' b0 c1 > 0
+    exact h_
+    rw [if_neg h_] at hS_and
+    contradiction
+  clear hS_and
+  obtain hS_and3 := Nat.pos_of_mul_pos_left hS_and2
+  obtain hS_and4 := Nat.pos_of_mul_pos_right hS_and2
+  clear hS_and2
+  exists b0
+  constructor
+  exact hkey
+  constructor
+  exact hR
+  rw [if_pos]
+  apply Nat.zero_lt_one
+  rw [gt_iff_lt]
+  apply Nat.mul_pos
+  exact hS_and4
+  exact hS_and3
 
 -- 895: singleton keys fixes the witness, so right-AND distributes exactly.
 theorem ex895 (b : B) (R : WRel A B) (S S' : WRel B C) :
     wReachComp [b] R (wAnd S S')
       =
     wAnd (wReachComp [b] R S) (wReachComp [b] R S') := by
-  -- TODO
   -- Hint: use ex867 to rewrite both sides to mask forms with the same b.
-  sorry
+  funext a1 c1
+  dsimp [wReachComp, wAnd, wBool, wSupp, wMul, maskW, relCompList]
+  by_cases h1 : ∃ b_1, b_1 ∈ [b] ∧ R a1 b_1 > 0 ∧ (if S b_1 c1 * S' b_1 c1 > 0 then 1 else 0) > 0
+  -- pos
+  obtain ⟨b0, hkey, hR, hS_and⟩ := h1
+  have hS_and2 : S b0 c1 * S' b0 c1 > 0 := by
+    by_cases h_ : S b0 c1 * S' b0 c1 > 0
+    exact h_
+    rw [if_neg h_] at hS_and
+    contradiction
+  clear hS_and
+  obtain hS_and3 := Nat.pos_of_mul_pos_left hS_and2
+  obtain hS_and4 := Nat.pos_of_mul_pos_right hS_and2
+  clear hS_and2
+  rw [if_pos]
+  rw [if_pos]
+  rw [if_pos]
+  rw [Nat.one_mul]
+  rw [if_pos]
+  apply Nat.zero_lt_one
+  exists b0
+  exists b0
+  exists b0
+  constructor
+  exact hkey
+  constructor
+  exact hR
+  rw [if_pos]
+  apply Nat.zero_lt_one
+  rw [gt_iff_lt]
+  apply Nat.mul_pos
+  exact hS_and4
+  exact hS_and3
+  -- neg
+  rw [if_neg]
+  rw [if_neg]
+  intro h2
+  apply h1
+  rw [gt_iff_lt] at h2
+  obtain h2_1 := Nat.pos_of_mul_pos_right h2
+  obtain h2_2 := Nat.pos_of_mul_pos_left h2
+  clear h2
+  have h2_3 : ∃ b_1, b_1 ∈ [b] ∧ R a1 b_1 > 0 ∧ S b_1 c1 > 0 := by
+    by_cases h_ : ∃ b_1, b_1 ∈ [b] ∧ R a1 b_1 > 0 ∧ S b_1 c1 > 0
+    exact h_
+    rw [if_neg] at h2_1
+    contradiction
+    intro h2_
+    apply h_
+    obtain ⟨b_1, hkey, hR, hS⟩ := h2_
+    exists b_1
+  clear h2_1
+  obtain ⟨b1,h2_3_1,h2_3_2,h2_3_3⟩ := h2_3
+  rw [List.mem_singleton] at h2_3_1
+  rw [h2_3_1] at h2_3_2
+  rw [h2_3_1] at h2_3_3
+  have h2_4 : ∃ b_1, b_1 ∈ [b] ∧ R a1 b_1 > 0 ∧ S' b_1 c1 > 0 := by
+    by_cases h_ : ∃ b_1, b_1 ∈ [b] ∧ R a1 b_1 > 0 ∧ S' b_1 c1 > 0
+    exact h_
+    rw [if_neg] at h2_2
+    contradiction
+    intro h2_
+    apply h_
+    obtain ⟨b_1, hkey, hR, hS'⟩ := h2_
+    exists b_1
+  clear h2_2
+  obtain ⟨b2,h2_4_1,h2_4_2,h2_4_3⟩ := h2_4
+  rw [List.mem_singleton] at h2_4_1
+  rw [h2_4_1] at h2_4_2
+  rw [h2_4_1] at h2_4_3
+  exists b
+  constructor
+  rw [List.mem_singleton]
+  constructor
+  exact h2_3_2
+  rw [if_pos]
+  apply Nat.zero_lt_one
+  rw [gt_iff_lt]
+  apply Nat.mul_pos
+  exact h2_3_3
+  exact h2_4_3
+  intro h3
+  apply h1
+  obtain ⟨b0, hkey, hR, hS_and⟩ := h3
+  rw [List.mem_singleton] at hkey
+  rw [hkey] at hR
+  rw [hkey] at hS_and
+  have hS_and2 : S b c1 * S' b c1 > 0 := by
+    by_cases h_ : S b c1 * S' b c1 > 0
+    exact h_
+    rw [if_neg h_] at hS_and
+    contradiction
+  clear hS_and
+  obtain hS_and3 := Nat.pos_of_mul_pos_left hS_and2
+  obtain hS_and4 := Nat.pos_of_mul_pos_right hS_and2
+  clear hS_and2
+  exists b
+  constructor
+  rw [List.mem_singleton]
+  constructor
+  exact hR
+  rw [if_pos]
+  apply Nat.zero_lt_one
+  rw [gt_iff_lt]
+  apply Nat.mul_pos
+  exact hS_and4
+  exact hS_and3
 
 --------------------------------------------------------------------------------
 -- 896-900: wsumW / graph exercises with wAnd
